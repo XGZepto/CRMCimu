ff098c5d73a9412ab3c8d4f313cddbaed4aaf610
Zepto <xgzepto@gmail.com>
2025-07-25 18:08:29 -0400

    shacdn

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import './styles.css'
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
   title: 'Payload Blank Template',
 }
+
+import '@/styles/global.css'
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
